f49d846a81930d787af02198a50cddbcc6af724c
sozdemirr <selenay.zdemir@outlook.com>
2023-08-30 23:42:06 +0300

    Pyhton Basics

---
+++ pyhton.py
@@ -0,0 +1,257 @@
+5+6
+
+900/8
+
+38*6
+
+2*2*2
+
+2**3
+
+2 ** 10
+
+10 % 2
+
+11%3
+
+11%2
+
+Integer
+
+3 + 3
+
+Float
+
+0.4 * 3
+
+3 + 5
+
+Variables
+
+x=3
+
+y = 5
+
+x + y
+
+x**y
+
+x=4
+
+x + y 
+
+r= input('r:')
+
+r*2*3.14
+
+type(x)
+
+pi = 3.14
+
+type(pi)
+
+r
+
+'5' tırnak içinde olduğu için bu bir metin yani tırnağın içindeki bir isim de olabilir o yüzden r yi sayı olarak almadı ve işlemi yapmadı.
+
+
+type(r)
+
+str yani string= metin, değişken
+
+integer ve float çarpıabilir
+
+x*pi
+
+ama string ile float ya da integer çarpılmaz çünkü isim olabilir string ama r'den bir integer oluşturmak için yeni bir değişken oluşturabilinir.
+
+r_int=int(r)
+
+r_int
+
+type(r_int)
+
+r_int*2*pi
+
+şimdi işlem oldu
+
+"hello world"
+
+'hello world'
+
+çift tırnakla yazmak daha iyi.
+
+"I'm a pilot"
+
+x="hello world"
+
+x
+
+x="hello"
+
+x
+
+x=3
+
+x
+
+pyhton'da önce string olarak tanımladığımız şeyi sonra integer olarak tanımlayabiliriz.
+
+x="hello world"
+
+type(x)
+
+len(x)
+
+length bize x'in uzunluğunu veriyor. İki tırnak işaretinin içindeki her şeyi sayıyor boşluklarda dahil.
+
+print(x)
+
+print("hello world")
+
+Escape Characters
+
+print("hello pyhton")
+
+print("hello
+      pyhton")
+
+iki satırda yapmak istiyorsak \n kullanılmalı.
+
+print ("hello \npyhton")
+
+print("hello \t pyhton")
+
+my_string="hello world"
+
+my_string
+
+##indexing
+
+my_string[0]
+
+my_string[5]
+
+my_string[6]
+
+my_string[4]
+
+my_string[-1]
+
+my_string[-2]
+
+my_string_2 = "1234567890"
+
+my_string_2[0]
+
+##slicing
+
+my_string_2[2:]
+
+2.indeks ve sonrasını gösterir.
+
+my_string_2[4:]
+
+4. indeks ve sonrasını gösterir.
+
+##stopping index
+
+my_string_2[:2]
+
+2. indekste durur ve 2. indeks öncesini gösterir.
+
+my_string_2[:4]
+
+4. indekste durur ve 4. indeks öncesini gösterir.
+
+my_string_2[9:]
+
+my_string_2[:0]
+
+my_string_2[2:4]
+
+my_string_2[2:3]
+
+my_string_2[5:8]
+
+##step size
+
+my_string_2[::]
+
+my_string_2[::3]
+
+3'er 3'er git demek
+
+my_string_2[::2]
+
+my_string_2[2:8:2]
+
+
+2. indeksten başlayacak 8. indekste duracak ve 2'şer 2'şer olacak
+
+my_string_2[1:7:3]
+
+1. indeksten başlayacak 7. indekste duracak ve 3'er 3'er olacak.
+
+my_string_2[: :-1]
+
+## string methods
+
+my_name= "selenay"
+
+my_name.capitalize()
+
+my_name
+
+my_name_capitalize=my_name.capitalize()
+
+my_name_capitalize
+
+my_name= "selenay özdemir"
+
+my_name.split()
+
+bu bir liste
+
+my_name
+
+my_name_split=my_name.split()
+
+my_name_split
+
+my_name_split[0]
+
+my_name_split[1]
+
+"james"*10*2
+
+string ile integer  çarpılınca 20 kere stringi yazıyor ama diğer işlemler yapılamıyor ve string ile float da hiç bir işlem yapılmaz
+
+"james"+5
+
+"james"+"5"
+
+"james"+ "black"
+
+
+my_name="selenay"
+
+my_surname=" özdemir"
+
+my_name+my_surname
+
+my_full_name= my_name+my_surname
+
+my_full_name
+
+
+
+
+
+
+
+
+
+
+
+
